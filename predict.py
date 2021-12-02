09f80b78c603ea0af9a5940489c45874ea6bbcfc
Angelly Cabrera <karycabr@usc.edu>
2021-12-01 21:37:58 -0800

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,18 @@
+import pickle
+from flask import Flask,request
+from flask import jsonify
+import json
+#import soundsensor
+
+clf = pickle.load(open('model.pickle','rb'))
+
+app = Flask(__name__)
+#Want volume to come from the raspberry pi
+
+@app.route("/classify")
+def hello_world():
+
+   volume = pickle.load(open("sound.pickle", "rb"))
+   genre = clf.predict([[float(volume)]])[0]
+   
+   return json.dumps({"genre" : genre})
